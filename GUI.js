7f78b373d7116dcf3530102e5ebd51e9de876fa8
CodeMaster633 <126244327+CodeMaster633@users.noreply.github.com>
2024-03-06 12:29:07 +0100

    yes

--- GUI.js
+++ GUI.js
@@ -13,15 +13,20 @@ import {
 let input = document.querySelector("input");
 let terningeBilleder = document.getElementsByClassName("terning");
 let button = document.getElementById("kastTerninger");
+let slagString = document.getElementById("slagString");
 let myHoldArray = [true, true, true, true, true];
+let slagNr = 0;
 
 terningSetup();
 
 button.onclick = function () {
-  console.log("Kastet");
   let slag = kastTerning(myHoldArray);
   terningeBillederVis(slag, myHoldArray);
   terningSetup();
+
+  slagNr++;
+  slagString.innerHTML = `Slag nr ${slagNr}`;
+  console.log(slagNr);
 };
 
 function terningeBillederVis(slag, holdArray) {
@@ -45,8 +50,13 @@ function terningeBillederVis(slag, holdArray) {
 function terningSetup() {
   Array.from(terningeBilleder).forEach((element) => {
     element.onclick = function () {
-      element.style = "filter: opacity(50%);";
-      myHoldArray[parseInt(element.id.charAt(3)) - 1] = false;
+      if (myHoldArray[parseInt(element.id.charAt(3)) - 1]) {
+        element.style = "filter: opacity(50%);";
+        myHoldArray[parseInt(element.id.charAt(3)) - 1] = false;
+      } else {
+        element.style = "";
+        myHoldArray[parseInt(element.id.charAt(3)) - 1] = true;
+      }
       console.log(myHoldArray);
     };
   });
