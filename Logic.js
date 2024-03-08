e2777955e517ab7e46e8a55cf841c61e89b8ba58
Loshan Sundaramoorthy <losh0001@gmail.com>
2024-03-09 00:05:25 +0100

    Lidt hist og pist.

--- Logic.js
+++ Logic.js
@@ -1,212 +1,212 @@
 export let nuværendeSlag = [1, 1, 1, 1, 1];
 
 export function kastTerning(holdArray) {
-  //let slag = [];
-  let i = 0;
+    //let slag = [];
+    let i = 0;
 
-  holdArray.forEach((element) => {
-    if (element == true) {
-      nuværendeSlag[i] = Math.floor(Math.random() * 6 + 1);
-    }
-    i++;
-  });
+    holdArray.forEach((element) => {
+        if (element == true) {
+            nuværendeSlag[i] = Math.floor(Math.random() * 6 + 1);
+        }
+        i++;
+    });
 
-  console.log(nuværendeSlag);
-  return nuværendeSlag;
+    console.log(nuværendeSlag);
+    return nuværendeSlag;
 }
 
 export function frequency(dice) {
-  const freq = Array(7).fill(0);
-  dice.forEach((value) => {
-    freq[value]++;
-  });
-  return freq;
+    const freq = Array(7).fill(0);
+    dice.forEach((value) => {
+        freq[value]++;
+    });
+    return freq;
 }
 
 export function ones(dice) {
-  const freq = frequency(dice);
-  console.log(freq);
-  return freq[1] * 1;
+    const freq = frequency(dice);
+    console.log(freq);
+    return freq[1] * 1;
 }
 
 export function twos(dice) {
-  const freq = frequency(dice);
-  console.log(freq);
-  return freq[2] * 2;
+    const freq = frequency(dice);
+    console.log(freq);
+    return freq[2] * 2;
 }
 
 export function threes(dice) {
-  const freq = frequency(dice);
-  console.log(freq);
-  return freq[3] * 3;
+    const freq = frequency(dice);
+    console.log(freq);
+    return freq[3] * 3;
 }
 
 export function fours(dice) {
-  const freq = frequency(dice);
-  console.log(freq);
-  return freq[4] * 4;
+    const freq = frequency(dice);
+    console.log(freq);
+    return freq[4] * 4;
 }
 
 export function fives(dice) {
-  const freq = frequency(dice);
-  console.log(freq);
-  return freq[5] * 5;
+    const freq = frequency(dice);
+    console.log(freq);
+    return freq[5] * 5;
 }
 
 export function sixs(dice) {
-  const freq = frequency(dice);
-  console.log(freq);
-  return freq[6] * 6;
+    const freq = frequency(dice);
+    console.log(freq);
+    return freq[6] * 6;
 }
 
 export function onePairPoints(dice) {
-  const freq = frequency(dice);
-  let sum = 0;
+    const freq = frequency(dice);
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 2) {
-      sum = i * 2;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 2) {
+            sum = i * 2;
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function twoPairPoints(dice) {
-  const freq = frequency(dice);
-  let pairs = 0;
-  let sum = 0;
+    const freq = frequency(dice);
+    let pairs = 0;
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 2) {
-      sum += i * 2;
-      pairs++;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 2) {
+            sum += i * 2;
+            pairs++;
+        }
     }
-  }
 
-  if (pairs == 2) {
-    return sum;
-  }
+    if (pairs == 2) {
+        return sum;
+    }
 
-  return 0;
+    return 0;
 }
 
 export function threeSamePoints(dice) {
-  const freq = frequency(dice);
-  let sum = 0;
+    const freq = frequency(dice);
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 3) {
-      sum += i * 3;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 3) {
+            sum += i * 3;
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function fourSamePoints(dice) {
-  const freq = frequency(dice);
-  let pairs = 0;
-  let sum = 0;
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 4) {
-      sum += i * 4;
+    const freq = frequency(dice);
+    let pairs = 0;
+    let sum = 0;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 4) {
+            sum += i * 4;
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function fullHousePoints(dice) {
-  const freq = frequency(dice);
-  let sum = 0;
-  let foundThree = false;
-  let foundTwo = false;
+    const freq = frequency(dice);
+    let sum = 0;
+    let foundThree = false;
+    let foundTwo = false;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] == 3) {
-      foundThree = true;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] == 3) {
+            foundThree = true;
+        }
+        if (freq[i] == 2) {
+            foundTwo = true;
+        }
     }
-    if (freq[i] == 2) {
-      foundTwo = true;
-    }
-  }
 
-  if (foundThree && foundTwo) {
-    for (let i = 0; i < dice.length; i++) {
-      sum += dice[i];
+    if (foundThree && foundTwo) {
+        for (let i = 0; i < dice.length; i++) {
+            sum += dice[i];
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function smallStraightPoints(dice) {
-  const freq = frequency(dice);
-  let duplicates = 0;
-  let sum = 0;
+    const freq = frequency(dice);
+    let duplicates = 0;
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] == 2) {
-      duplicates++;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] == 2) {
+            duplicates++;
+        }
+        sum = sum + freq[i] * i;
     }
-    sum = sum + freq[i] * i;
-  }
 
-  if (sum == 15 && duplicates < 2) {
-    return sum;
-  } else {
-    return 0;
-  }
+    if (sum == 15 && duplicates < 2) {
+        return sum;
+    } else {
+        return 0;
+    }
 }
 
 //Skal ændres TODO
 export function largeStraightPoints(dice) {
-  const freq = frequency(dice);
-  let duplicates = 0;
-  let sum = 0;
+    const freq = frequency(dice);
+    let duplicates = 0;
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] == 2) {
-      duplicates++;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] == 2) {
+            duplicates++;
+        }
+        sum = sum + freq[i] * i;
     }
-    sum = sum + freq[i] * i;
-  }
 
-  if (sum == 20 && duplicates < 2) {
-    return sum;
-  } else {
-    return 0;
-  }
+    if (sum == 20 && duplicates < 2) {
+        return sum;
+    } else {
+        return 0;
+    }
 }
 
 export function chancePoints(dice) {
-  let sum = 0;
+    let sum = 0;
 
-  for (let i = 0; i < dice.length; i++) {
-    sum += dice[i];
-  }
+    for (let i = 0; i < dice.length; i++) {
+        sum += dice[i];
+    }
 
-  return sum;
+    return sum;
 }
 
 export function yatzyPoints(dice) {
-  let sum = 0;
-  let first = dice[0];
-  let yatzy = false;
+    let sum = 0;
+    let first = dice[0];
+    let yatzy = false;
 
-  for (let i = 0; i < dice.length; i++) {
-    if (dice[i] != first) {
-      yatzy = false;
-      break;
-    } else {
-      yatzy = true;
+    for (let i = 0; i < dice.length; i++) {
+        if (dice[i] != first) {
+            yatzy = false;
+            break;
+        } else {
+            yatzy = true;
+        }
     }
-  }
 
-  if (yatzy) {
-    sum = 50;
-  }
+    if (yatzy) {
+        sum = 50;
+    }
 
-  return sum;
+    return sum;
 }
