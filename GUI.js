2ca2317f601ad2b9f1ba030fd4b89ee1575aaae6
Loshan Sundaramoorthy <losh0001@gmail.com>
2024-03-11 20:50:17 +0100

    Initial commit

--- GUI.js
+++ GUI.js
@@ -1,35 +1,37 @@
 import {
-  kastTerning,
-  vælgInputFelt,
-  getTotal,
-  ones,
-  twos,
-  threes,
-  fours,
-  fives,
-  sixs,
-  onePairPoints,
-  twoPairPoints,
-  threeSamePoints,
-  fourSamePoints,
-  fullHousePoints,
-  smallStraightPoints,
-  largeStraightPoints,
-  chancePoints,
-  yatzyPoints,
-  nuværendeSlag,
-  resetSlag,
-  addToSum,
-  getSum,
-  getBonus,
+    kastTerning,
+    vælgInputFelt,
+    getTotal,
+    ones,
+    twos,
+    threes,
+    fours,
+    fives,
+    sixs,
+    onePairPoints,
+    twoPairPoints,
+    threeSamePoints,
+    fourSamePoints,
+    fullHousePoints,
+    smallStraightPoints,
+    largeStraightPoints,
+    chancePoints,
+    yatzyPoints,
+    nuværendeSlag,
+    resetSlag,
+    addToSum,
+    getSum,
+    getBonus,
+     //resetSpil - resetSpil fungerer ikke, metoderne er nederst i GUI, og i Logic klassen, 
+     // Tag gerne et kig.
 } from "./Logic.js";
 
 let terningeBilleder = document.getElementsByClassName("terning");
 let button = document.getElementById("kastTerninger");
 let slagString = document.getElementById("slagString");
 let pointfelter = document
-  .getElementById("pointDiv")
-  .getElementsByClassName("pointFelt");
+    .getElementById("pointDiv")
+    .getElementsByClassName("pointFelt");
 let totalFelt = document.getElementById("total");
 let sumFelt = document.getElementById("sum");
 let bonusFelt = document.getElementById("bonus");
@@ -43,183 +45,186 @@ updateTotal();
 updateBonus();
 
 button.onclick = function () {
-  let slag = kastTerning(myHoldArray);
-  terningeBillederVis(slag, myHoldArray);
-  terningSetup();
-  opdaterPointfelter();
+    let slag = kastTerning(myHoldArray);
+    terningeBillederVis(slag, myHoldArray);
+    terningSetup();
+    opdaterPointfelter();
 
-  slagNr++;
-  updateSlagString();
-  stopVedTreSlag();
+    slagNr++;
+    updateSlagString();
+    stopVedTreSlag();
 };
 
 function terningeBillederVis(slag, holdArray) {
-  terningeBilleder.innerHTML = "";
-  let i = 0;
-
-  slag.forEach((element) => {
-    if (holdArray[i]) {
-      terningeBilleder[i].outerHTML = `<img
-    src="img/terning${element}.png"
-    width="100"
-    height="100"
-    class="terning"
-    id="img${i + 1}"
-    />`;
-    }
-    i++;
-  });
+    slag.forEach((value, i) => {
+        let terningElement = terningeBilleder[i];
+        terningElement.src = `img/terning${value}.png`;
+        terningElement.style.opacity = holdArray[i] ? "1" : "0.5";
+    });
 }
 
 function terningSetup() {
-  Array.from(terningeBilleder).forEach((element) => {
-    element.onclick = function () {
-      if (myHoldArray[parseInt(element.id.charAt(3)) - 1]) {
-        element.style = "filter: opacity(50%);";
-        myHoldArray[parseInt(element.id.charAt(3)) - 1] = false;
-      } else {
-        element.style = "";
-        myHoldArray[parseInt(element.id.charAt(3)) - 1] = true;
-      }
-    };
-  });
+    Array.from(terningeBilleder).forEach((element, i) => {
+        element.addEventListener('click', () => {
+            myHoldArray[i] = !myHoldArray[i];
+            element.style.opacity = myHoldArray[i] ? "1" : "0.5";
+        });
+    });
 }
 
 function stopVedTreSlag() {
-  if (slagNr >= 3) {
-    button.disabled = true;
-  }
+    if (slagNr >= 3) {
+        button.disabled = true;
+    }
 }
 
 function kastTilgængeligIgen() {
-  button.disabled = false;
+    button.disabled = false;
 }
 
 function opdaterPointfelter() {
-  Array.from(pointfelter).forEach((element) => {
-    switch (element.id) {
-      case "ones":
-        if (element.disabled != true) {
-          element.value = ones(nuværendeSlag);
+    Array.from(pointfelter).forEach((element) => {
+        switch (element.id) {
+            case "ones":
+                if (element.disabled != true) {
+                    element.value = ones(nuværendeSlag);
+                }
+                break;
+            case "twos":
+                if (element.disabled != true) {
+                    element.value = twos(nuværendeSlag);
+                }
+                break;
+            case "threes":
+                if (element.disabled != true) {
+                    element.value = threes(nuværendeSlag);
+                }
+                break;
+            case "fours":
+                if (element.disabled != true) {
+                    element.value = fours(nuværendeSlag);
+                }
+                break;
+            case "fives":
+                if (element.disabled != true) {
+                    element.value = fives(nuværendeSlag);
+                }
+                break;
+            case "sixs":
+                if (element.disabled != true) {
+                    element.value = sixs(nuværendeSlag);
+                }
+                break;
+            case "onePair":
+                if (element.disabled != true) {
+                    element.value = onePairPoints(nuværendeSlag);
+                }
+                break;
+            case "twoPair":
+                if (element.disabled != true) {
+                    element.value = twoPairPoints(nuværendeSlag);
+                }
+                break;
+            case "threeOfAKind":
+                if (element.disabled != true) {
+                    element.value = threeSamePoints(nuværendeSlag);
+                }
+                break;
+            case "fourOfAKind":
+                if (element.disabled != true) {
+                    element.value = fourSamePoints(nuværendeSlag);
+                }
+                break;
+            case "fullHouse":
+                if (element.disabled != true) {
+                    element.value = fullHousePoints(nuværendeSlag);
+                }
+                break;
+            case "smallStraight":
+                if (element.disabled != true) {
+                    element.value = smallStraightPoints(nuværendeSlag);
+                }
+                break;
+            case "largeStraight":
+                if (element.disabled != true) {
+                    element.value = largeStraightPoints(nuværendeSlag);
+                }
+                break;
+            case "chance":
+                if (element.disabled != true) {
+                    element.value = chancePoints(nuværendeSlag);
+                }
+                break;
+            case "yatzy":
+                if (element.disabled != true) {
+                    element.value = yatzyPoints(nuværendeSlag);
+                }
+                break;
         }
-        break;
-      case "twos":
-        if (element.disabled != true) {
-          element.value = twos(nuværendeSlag);
-        }
-        break;
-      case "threes":
-        if (element.disabled != true) {
-          element.value = threes(nuværendeSlag);
-        }
-        break;
-      case "fours":
-        if (element.disabled != true) {
-          element.value = fours(nuværendeSlag);
-        }
-        break;
-      case "fives":
-        if (element.disabled != true) {
-          element.value = fives(nuværendeSlag);
-        }
-        break;
-      case "sixs":
-        if (element.disabled != true) {
-          element.value = sixs(nuværendeSlag);
-        }
-        break;
-      case "onePair":
-        if (element.disabled != true) {
-          element.value = onePairPoints(nuværendeSlag);
-        }
-        break;
-      case "twoPair":
-        if (element.disabled != true) {
-          element.value = twoPairPoints(nuværendeSlag);
-        }
-        break;
-      case "threeOfAKind":
-        if (element.disabled != true) {
-          element.value = threeSamePoints(nuværendeSlag);
-        }
-        break;
-      case "fourOfAKind":
-        if (element.disabled != true) {
-          element.value = fourSamePoints(nuværendeSlag);
-        }
-        break;
-      case "fullHouse":
-        if (element.disabled != true) {
-          element.value = fullHousePoints(nuværendeSlag);
-        }
-        break;
-      case "smallStraight":
-        if (element.disabled != true) {
-          element.value = smallStraightPoints(nuværendeSlag);
-        }
-        break;
-      case "largeStraight":
-        if (element.disabled != true) {
-          element.value = largeStraightPoints(nuværendeSlag);
-        }
-        break;
-      case "chance":
-        if (element.disabled != true) {
-          element.value = chancePoints(nuværendeSlag);
-        }
-        break;
-      case "yatzy":
-        if (element.disabled != true) {
-          element.value = yatzyPoints(nuværendeSlag);
-        }
-        break;
-    }
-  });
+    });
 }
 
 function pointfelterSetup() {
-  Array.from(pointfelter).forEach((element) => {
-    element.onclick = function () {
-      vælgInputFelt(parseInt(this.value));
-      if (
-        element.id == "ones" ||
-        element.id == "twos" ||
-        element.id == "threes" ||
-        element.id == "fours" ||
-        element.id == "fives" ||
-        element.id == "sixs"
-      ) {
-        addToSum(parseInt(this.value));
-      }
-      updateBonus();
-      updateTotal();
-      updateSum();
-      resetTerninger();
-      element.disabled = "disabled";
-    };
-  });
+    Array.from(pointfelter).forEach((element) => {
+        element.onclick = function () {
+            vælgInputFelt(parseInt(this.value));
+            if (
+                element.id == "ones" ||
+                element.id == "twos" ||
+                element.id == "threes" ||
+                element.id == "fours" ||
+                element.id == "fives" ||
+                element.id == "sixs"
+            ) {
+                addToSum(parseInt(this.value));
+            }
+            updateBonus();
+            updateTotal();
+            updateSum();
+            resetTerninger();
+            element.disabled = "disabled";
+        };
+    });
 }
 
 function updateTotal() {
-  totalFelt.value = getTotal();
+    totalFelt.value = getTotal();
 }
 
 function updateSum() {
-  sumFelt.value = getSum();
+    sumFelt.value = getSum();
 }
 
 function updateBonus() {
-  bonusFelt.value = getBonus();
+    bonusFelt.value = getBonus();
 }
 
 function updateSlagString() {
-  slagString.innerHTML = `Slag nr ${slagNr}`;
+    slagString.innerHTML = `Slag nr ${slagNr}`;
 }
 
 function resetTerninger() {
-  myHoldArray = [true, true, true, true, true];
-  slagNr = 0;
-  updateSlagString();
-  terningeBillederVis(resetSlag(), myHoldArray);
-  kastTilgængeligIgen();
+    myHoldArray = [true, true, true, true, true];
+    slagNr = 0;
+    updateSlagString();
+    terningeBillederVis(resetSlag(), myHoldArray);
+    kastTilgængeligIgen();
 }
+
+function resetUI() {
+    resetTerninger();
+
+    Array.from(pointfelter).forEach(felt => {
+        felt.value = '';
+        felt.disabled = false;
+    });
+
+    totalFelt.value = '';
+    sumFelt.value = '';
+    bonusFelt.value = '';
+}
+
+document.getElementById('nulstilSpil').addEventListener('click', function() {
+    resetSpil();
+    resetUI();
+});
--- Logic.js
+++ Logic.js
@@ -4,233 +4,233 @@ export let sum = 0;
 export let bonus = 0;
 
 export function kastTerning(holdArray) {
-  let i = 0;
+    let i = 0;
 
-  holdArray.forEach((element) => {
-    if (element == true) {
-      nuværendeSlag[i] = Math.floor(Math.random() * 6 + 1);
-    }
-    i++;
-  });
+    holdArray.forEach((element) => {
+        if (element == true) {
+            nuværendeSlag[i] = Math.floor(Math.random() * 6 + 1);
+        }
+        i++;
+    });
 
-  return nuværendeSlag;
+    return nuværendeSlag;
 }
 
 export function vælgInputFelt(værdi) {
-  totalVærdi += værdi;
+    totalVærdi += værdi;
 }
 
 export function addToSum(værdi) {
-  sum += værdi;
+    sum += værdi;
+    if (sum >= 63 && bonus === 0) {
+        bonus = 50;
+        totalVærdi += bonus;
+    }
 }
 
 export function getTotal() {
-  return totalVærdi;
+    return totalVærdi;
 }
 
 export function getSum() {
-  return sum;
+    return sum;
 }
 
 export function getBonus() {
-  if (sum >= 63) {
-    bonus = 50;
-    totalVærdi += bonus;
-  }
-  return bonus;
+    if (sum >= 63) {
+        bonus = 50;
+        totalVærdi += bonus;
+    }
+    return bonus;
+}
+
+export function resetSpil() {
+    nuværendeSlag.fill(1);
+    totalVærdi = 0;
+    sum = 0;
+    bonus = 0;
 }
 
 export function resetSlag() {
-  nuværendeSlag = [1, 1, 1, 1, 1];
-  return nuværendeSlag;
+    nuværendeSlag = [1, 1, 1, 1, 1];
+    return nuværendeSlag;
 }
 
 export function frequency(dice) {
-  const freq = Array(7).fill(0);
-  dice.forEach((value) => {
-    freq[value]++;
-  });
-  return freq;
+    const freq = Array(7).fill(0);
+    dice.forEach((value) => {
+        freq[value]++;
+    });
+    return freq;
 }
 
 export function ones(dice) {
-  const freq = frequency(dice);
-  return freq[1] * 1;
+    const freq = frequency(dice);
+    return freq[1] * 1;
 }
 
 export function twos(dice) {
-  const freq = frequency(dice);
-  return freq[2] * 2;
+    const freq = frequency(dice);
+    return freq[2] * 2;
 }
 
 export function threes(dice) {
-  const freq = frequency(dice);
-  return freq[3] * 3;
+    const freq = frequency(dice);
+    return freq[3] * 3;
 }
 
 export function fours(dice) {
-  const freq = frequency(dice);
-  return freq[4] * 4;
+    const freq = frequency(dice);
+    return freq[4] * 4;
 }
 
 export function fives(dice) {
-  const freq = frequency(dice);
-  return freq[5] * 5;
+    const freq = frequency(dice);
+    return freq[5] * 5;
 }
 
 export function sixs(dice) {
-  const freq = frequency(dice);
-  return freq[6] * 6;
+    const freq = frequency(dice);
+    return freq[6] * 6;
 }
 
 export function onePairPoints(dice) {
-  const freq = frequency(dice);
-  let sum = 0;
+    const freq = frequency(dice);
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 2) {
-      sum = i * 2;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 2) {
+            sum = i * 2;
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function twoPairPoints(dice) {
-  const freq = frequency(dice);
-  let pairs = 0;
-  let sum = 0;
+    const freq = frequency(dice);
+    let pairs = 0;
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 2) {
-      sum += i * 2;
-      pairs++;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 2) {
+            sum += i * 2;
+            pairs++;
+        }
     }
-  }
 
-  if (pairs == 2) {
-    return sum;
-  }
+    if (pairs == 2) {
+        return sum;
+    }
 
-  return 0;
+    return 0;
 }
 
 export function threeSamePoints(dice) {
-  const freq = frequency(dice);
-  let sum = 0;
+    const freq = frequency(dice);
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 3) {
-      sum += i * 3;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 3) {
+            sum += i * 3;
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function fourSamePoints(dice) {
-  const freq = frequency(dice);
-  let pairs = 0;
-  let sum = 0;
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] >= 4) {
-      sum += i * 4;
+    const freq = frequency(dice);
+    let pairs = 0;
+    let sum = 0;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] >= 4) {
+            sum += i * 4;
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function fullHousePoints(dice) {
-  const freq = frequency(dice);
-  let sum = 0;
-  let foundThree = false;
-  let foundTwo = false;
+    const freq = frequency(dice);
+    let sum = 0;
+    let foundThree = false;
+    let foundTwo = false;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] == 3) {
-      foundThree = true;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] == 3) {
+            foundThree = true;
+        }
+        if (freq[i] == 2) {
+            foundTwo = true;
+        }
     }
-    if (freq[i] == 2) {
-      foundTwo = true;
-    }
-  }
 
-  if (foundThree && foundTwo) {
-    for (let i = 0; i < dice.length; i++) {
-      sum += dice[i];
+    if (foundThree && foundTwo) {
+        for (let i = 0; i < dice.length; i++) {
+            sum += dice[i];
+        }
     }
-  }
 
-  return sum;
+    return sum;
 }
 
 export function smallStraightPoints(dice) {
-  const freq = frequency(dice);
-  let duplicates = 0;
-  let sum = 0;
+    const freq = frequency(dice);
+    let duplicates = 0;
+    let sum = 0;
 
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] == 2) {
-      duplicates++;
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] == 2) {
+            duplicates++;
+        }
+        sum = sum + freq[i] * i;
     }
-    sum = sum + freq[i] * i;
-  }
 
-  if (sum == 15 && duplicates < 2) {
-    return sum;
-  } else {
-    return 0;
-  }
+    if (sum == 15 && duplicates < 2) {
+        return sum;
+    } else {
+        return 0;
+    }
 }
 
 //Skal ændres TODO
 export function largeStraightPoints(dice) {
-  const freq = frequency(dice);
-  let duplicates = 0;
-  let sum = 0;
-
-  for (let i = 0; i < freq.length; i++) {
-    if (freq[i] == 2) {
-      duplicates++;
+    const freq = frequency(dice);
+    if (freq[2] === 1 && freq[3] === 1 && freq[4] === 1 && freq[5] === 1 && freq[6] === 1) {
+        return 20;
     }
-    sum = sum + freq[i] * i;
-  }
-
-  if (sum == 20 && duplicates < 2) {
-    return sum;
-  } else {
     return 0;
-  }
 }
 
 export function chancePoints(dice) {
-  let sum = 0;
+    let sum = 0;
 
-  for (let i = 0; i < dice.length; i++) {
-    sum += dice[i];
-  }
+    for (let i = 0; i < dice.length; i++) {
+        sum += dice[i];
+    }
 
-  return sum;
+    return sum;
 }
 
 export function yatzyPoints(dice) {
-  let sum = 0;
-  let first = dice[0];
-  let yatzy = false;
+    let sum = 0;
+    let first = dice[0];
+    let yatzy = false;
 
-  for (let i = 0; i < dice.length; i++) {
-    if (dice[i] != first) {
-      yatzy = false;
-      break;
-    } else {
-      yatzy = true;
+    for (let i = 0; i < dice.length; i++) {
+        if (dice[i] != first) {
+            yatzy = false;
+            break;
+        } else {
+            yatzy = true;
+        }
     }
-  }
 
-  if (yatzy) {
-    sum = 50;
-  }
+    if (yatzy) {
+        sum = 50;
+    }
 
-  return sum;
+    return sum;
 }
